03ab24c78b994280bfe0398a77ec05d42dce7c10
Johnny Urdin <urdin32@gmail.com>
2026-03-29 15:42:24 -0500

    Fix: forzar 8 digitos en adapter directamente

--- Usuario/adapter.py
+++ Usuario/adapter.py
@@ -1,7 +1,13 @@
+from django.utils.crypto import get_random_string
+
 from allauth.account.adapter import DefaultAccountAdapter
 
 
 class CustomAccountAdapter(DefaultAccountAdapter):
+
+    def generate_email_verification_code(self):
+        code = get_random_string(length=8, allowed_chars='0123456789')
+        return code[:4] + '-' + code[4:]
 
     def get_signup_redirect_url(self, request):
         return '/verificacion-exitosa/'
